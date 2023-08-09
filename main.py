7da03c99ef054480f657db7f817ef374531c5ddc
Eray Arslan <erayarslan2019@outlook.com>
2023-08-10 00:35:17 +0300

    change library pynput to keyboard

--- main.py
+++ main.py
@@ -1,21 +1,24 @@
 from pygame import mixer
-from pynput.keyboard import Key, Listener
+import keyboard
 
 import random
 import os
 
-file_path = os.getcwd()
-soundsList = os.listdir(file_path+"/sounds")
-soundsList.remove("diff") # Remove diff folder
+import config
 
+file_path = config.PATH
+soundsList = os.listdir(file_path + "/sounds")
+soundsList.remove("diff")  # Remove diff folder
 
-print("\n\n Press ""END"" to STOP \n\n")
+print("\n\n Press 'END' to STOP \n\n")
 print(" Listener Started")
-def on_press(key):
-    print(key)      
-    if key == Key.space:
+
+
+def on_key_release(e):
+    print(e.name)
+    if e.name == 'space':
         playSound("diff/yamete.mp3")
-    elif key == Key.enter:
+    elif e.name == 'enter':
         x = random.randint(1, 3)
         if x == 1:
             playSound("diff/hoo1.mp3")
@@ -23,16 +26,18 @@ def on_press(key):
             playSound("diff/hoo2.mp3")
         else:
             playSound("diff/hoo3.mp3")
-    if (key != Key.enter and key != Key.space and key !=Key.esc and key != Key.end):
-        playSound(random.choice(soundsList)) # Choose random mp3 sound  
-    
-    if (key == Key.end): # To stop program press "end" key
+    if e.name not in ['enter', 'space', 'esc', 'end']:
+        playSound(random.choice(soundsList))  # Choose random mp3 sound
+
+    if e.name == 'end':  # To stop program press "end" key
         return False
+
 
 def playSound(name):
     mixer.init()
-    mixer.music.load("sounds/"+name) # Music file can only be MP3
+    mixer.music.load(file_path + "/sounds/" + name)  # Music file can only be MP3
     mixer.music.play()
-    
-with Listener(on_press=on_press) as listener:
-    listener.join()
+
+
+keyboard.hook(on_key_release)
+keyboard.wait('end')
